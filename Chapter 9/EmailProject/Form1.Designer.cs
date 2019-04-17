7070a77f44ee5f803220481bbcce83af9afdf5a6
Sal <harbifa@gmail.com>
2019-04-16 22:03:05 -0500

    j

---
+++ Chapter 9/EmailProject/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace EmailProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.People = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // People
+            // 
+            this.People.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.People.FormattingEnabled = true;
+            this.People.ItemHeight = 20;
+            this.People.Location = new System.Drawing.Point(0, 0);
+            this.People.Name = "People";
+            this.People.Size = new System.Drawing.Size(439, 224);
+            this.People.TabIndex = 0;
+            this.People.SelectedIndexChanged += new System.EventHandler(this.People_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 333);
+            this.Controls.Add(this.People);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox People;
+    }
+}
+
